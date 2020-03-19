0eb0226b97bc9d80eeaba1fcb815cbe8596a9977
Jules DUMAS <jules_ox@hotmail.fr>
2020-03-19 19:44:47 +0100

    home page

--- layout/header.php
+++ layout/header.php
@@ -1,10 +1,11 @@
 <header>
             <nav>
                 <ul class="navbar">
-                    <li><a href="../page/index.php">A propos</a></li>
+                    <li><a href="../page/home.php">Home</a></li>
+                    <li><a href="../index.php">A propos</a></li>
                     <li><a href="../page/portfolio.php">Portfolio</a></li>
-                    <li><a href="article.php">Article</a></li>
-                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="../page/article.php">Article</a></li>
+                    <li><a href="../page/contact.php">Contact</a></li>
                 </ul>
             </nav>
           <h1 class="big_title">Roquet Raccoon</h1>
